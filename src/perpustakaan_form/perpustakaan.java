b7ec266c8fd41d13ee47bfff7e35ccc4c7fa3999
endahpermatasari <endahpermatasari24@gmail.com>
2016-09-25 22:57:36 +0700

    coba

---
+++ src/perpustakaan_form/perpustakaan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package perpustakaan_form;
+import java.awt.*;
+import javax.swing.*;
+/**
+ *
+ * @author YANET
+ */
+public class perpustakaan extends JFrame {
+  public static void main(String[] args) {
+  new login().setVisible(true);
+  }  
+}
